d0b3a31a2b773f24eec3a004b5f8c498c52e36e0
Ishtiaq Ahmad <ig020888@gmail.com>
2026-08-14 15:26:16 +0500

    Fix sidebar course title color by avoiding theme paragraph styles.

--- cta-lms.php
+++ cta-lms.php
@@ -20,7 +20,7 @@ if ( ! defined( 'CTA_PLUGIN_FILE' ) ) {
 }
 
 if ( ! defined( 'CTA_VERSION' ) ) {
-	define( 'CTA_VERSION', '1.0.192' );
+	define( 'CTA_VERSION', '1.0.193' );
 }
 
 if ( ! defined( 'CTA_PLUGIN_DIR' ) ) {
