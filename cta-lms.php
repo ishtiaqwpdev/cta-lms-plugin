b46818970045408e45f25538ae3d2f367ca21f77
Ishtiaq Ahmad <ig020888@gmail.com>
2026-08-14 15:29:41 +0500

    Force sidebar course title to white with inline style and stronger CSS overrides.

--- cta-lms.php
+++ cta-lms.php
@@ -20,7 +20,7 @@ if ( ! defined( 'CTA_PLUGIN_FILE' ) ) {
 }
 
 if ( ! defined( 'CTA_VERSION' ) ) {
-	define( 'CTA_VERSION', '1.0.193' );
+	define( 'CTA_VERSION', '1.0.194' );
 }
 
 if ( ! defined( 'CTA_PLUGIN_DIR' ) ) {
